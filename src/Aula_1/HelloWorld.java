2cb6ea7a984cf7bb9c83d448c6a17e33e0664c97
Miguel Silva <miguel.lvl14@gmail.com>
2017-02-16 13:23:08 +0000

    Primeiro Programa em Java, Hello World

---
+++ src/Aula_1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Miguel Silva on 16/02/2017.
+ */
+package Aula_1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
