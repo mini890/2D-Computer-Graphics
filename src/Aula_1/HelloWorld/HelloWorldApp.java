a349d0e5d42544c262db1da368bcd861da47d8ae
Miguel Silva <miguel.lvl14@gmail.com>
2017-02-16 14:04:48 +0000

    Adicionado Acesso a outro ficheiro. Utilização de construtores.

--- src/Aula_1/HelloWorld/HelloWorldApp.java
+++ src/Aula_1/HelloWorld/HelloWorldApp.java
@@ -1,10 +1,17 @@
+package Aula_1.HelloWorld;
+
 /**
  * Created by Miguel Silva on 16/02/2017.
  */
-package Aula_1.HelloWorld;
 
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Declarar o objeto
+        Ponto2D p;
+        p = new Ponto2D(3, 5);
+
+        //Impressão para a consola
+        System.out.println(p.x);
+        System.out.println(p.y);
     }
 }
---
+++ src/Aula_1/HelloWorld/Ponto2D.java
@@ -0,0 +1,24 @@
+package Aula_1.HelloWorld;
+
+/**
+ * Created by Miguel Silva on 16/02/2017.
+ */
+public class Ponto2D {
+
+    //Criação de Variáveis X e Y
+    int x;
+    int y;
+
+    //Criação de Construtor
+    Ponto2D() {
+        x = y = 0;
+    }
+
+    //Criação de 2º Construtor
+    Ponto2D(int x, int y1) {
+        //Usar this quando existe 2 métodos com o mesmo nome
+        this.x = x;
+        //Esta seria outra alternativa ao this
+        y = y1;
+    }
+}
