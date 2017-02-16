b8d042cfbda1fcc9107b9978b625e337ec7c89e1
Miguel Silva <miguel.lvl14@gmail.com>
2017-02-16 13:33:53 +0000

    Modificado package para melhor compreensão dos Exercicios das aulas

--- src/Aula_1/HelloWorld/HelloWorldApp.java
+++ src/Aula_1/HelloWorld/HelloWorldApp.java
@@ -1,9 +1,9 @@
 /**
  * Created by Miguel Silva on 16/02/2017.
  */
-package Aula_1;
+package Aula_1.HelloWorld;
 
-public class HelloWorld {
+public class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
