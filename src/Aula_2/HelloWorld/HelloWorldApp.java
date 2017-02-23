21f52fa1453fd24a627def57aced1c08ec53ca34
Miguel Silva <miguel.lvl14@gmail.com>
2017-02-23 12:55:18 +0000

    Adicionado Array e Impressão com formato

--- src/Aula_2/HelloWorld/HelloWorldApp.java
+++ src/Aula_2/HelloWorld/HelloWorldApp.java
@@ -24,5 +24,21 @@ public class HelloWorldApp {
         System.out.println(p2.getX());
         System.out.println(p2.getY());
         System.out.println(p2.getZ());
+        
+        System.out.println("Ponto3D Usando Array\n");
+
+        //Declaração de 1 Array com 3 Posições
+        Ponto3D[] pa = new Ponto3D[3];
+        //Posição 0 é ...
+        pa[0] = new Ponto3D(3,6, 9);
+        pa[1] = new Ponto3D(5,7, 11);
+        pa[2] = new Ponto3D(7,10, 15);
+        
+        //Ciclo for que percorre o array automáticamente
+        for (Ponto3D pTemp: pa) {
+        	//Format permite imprimir com um determinado formato, %d para inteiro, %f para float %1.2f para 2 casas décimais
+        	//e determinar á frente o que vai ser impresso por ordem de escrita 
+        	System.out.format("Ponto (%d,%d,%d)\n", pTemp.getX(), pTemp.getY(), pTemp.getZ());
+        }
     }
 }
