2d88570be606b40c11e4ba8947e63ff3b72ed08e
Miguel Silva <miguel.lvl14@gmail.com>
2017-04-21 07:28:20 +0100

    Update exercício Carro

--- src/Aula6/Carro/ResolucaoProfInicial/Carro.java
+++ src/Aula6/Carro/ResolucaoProfInicial/Carro.java
@@ -1,49 +1,83 @@
 package Aula6.Carro.ResolucaoProfInicial;
 
 import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
 import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Carro {
 
 	AffineTransform rotacaoRodas, posicao;
+	int comp, alt;
 	Roda rf, rt;
+	BufferedImage bi;
 
-	public Carro(int x, int y) {
+	public Carro(int x, int y, int c, int a) {
 		rotacaoRodas = new AffineTransform();
 		posicao = new AffineTransform();
 		posicao.translate(x, y);
-		rf = new RodaAros(40, 16);
-		rt = new RodaAros(40, 16);
+		rf = new RodaAros(30, 16);
+		rt = new RodaAros(30, 16);
+
+		comp = c;
+		alt = a;
+
+		try {
+			bi = ImageIO.read(new File("src/Aula6/Carro/ResolucaoProfInicial/fundo.jpg"));
+		} catch (IOException e) {
+			System.out.println("Erro ao carregar o ficheiro.");
+		}
 	}
 
 	public void desenha(Graphics2D g2d) {
-
 		g2d.setTransform(posicao);
+
+		Paint paintInicial = g2d.getPaint();
+		// GradientPaint g = new GradientPaint(0, -alt / 4, Color.PINK, 0, alt /
+		// 4, Color.YELLOW, true);
+		TexturePaint g = new TexturePaint(bi, new Rectangle(0, 0, bi.getWidth(), bi.getHeight()));
+		g2d.setPaint(g);
+		g2d.fillRoundRect(-comp / 2, -alt / 2, comp, alt, 5, 5);
+
+		g2d.setPaint(paintInicial);
+		g2d.drawRoundRect(-comp / 2, -alt / 2, comp, alt, 5, 5);
+
 		AffineTransform eixoParaGuardar = g2d.getTransform();
 		AffineTransform eixoParaTransformar = g2d.getTransform();
 
-		eixoParaTransformar.concatenate(posicao);
+		eixoParaTransformar.translate(-comp / 4, alt / 2);
 		eixoParaTransformar.concatenate(rotacaoRodas);
 		g2d.setTransform(eixoParaTransformar);
-
 		rf.desenha(g2d);
 
-		eixoParaGuardar.translate(150, 0);
-		eixoParaGuardar.concatenate(posicao);
+		eixoParaGuardar.translate(comp / 4, alt / 2);
 		eixoParaGuardar.concatenate(rotacaoRodas);
 		g2d.setTransform(eixoParaGuardar);
-
 		rt.desenha(g2d);
 	}
 
 	public void avanca() {
 		rotacaoRodas.rotate(Math.toRadians(-10));
-		posicao.translate(-3, 0);
+		posicao.translate(-5, 0);
 	}
 
 	public void recua() {
 		rotacaoRodas.rotate(Math.toRadians(10));
-		posicao.translate(3, 0);
+		posicao.translate(5, 0);
+	}
+
+	public void sobe() {
+		posicao.rotate(Math.toRadians(-2));
+	}
+
+	public void desce() {
+		posicao.rotate(Math.toRadians(2));
 	}
 
 }
--- src/Aula6/Carro/ResolucaoProfInicial/Tela.java
+++ src/Aula6/Carro/ResolucaoProfInicial/Tela.java
@@ -10,30 +10,51 @@ import javax.swing.JPanel;
 
 public class Tela extends JPanel implements KeyListener {
 
-	Carro c;
+	Carro c1, c2;
 
 	Tela() {
 		setFocusable(true);
 		addKeyListener(this);
 
-		c = new Carro(100, 100);
+		c1 = new Carro(100, 100, 200, 80);
+		c2 = new Carro(100, 300, 250, 100);
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
 
-		c.desenha(g2d);
+		c1.desenha(g2d);
+		c2.desenha(g2d);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_LEFT:
-			c.avanca();
+			c1.avanca();
 			break;
 		case KeyEvent.VK_RIGHT:
-			c.recua();
+			c1.recua();
+			break;
+		case KeyEvent.VK_UP:
+			c1.sobe();
+			break;
+		case KeyEvent.VK_DOWN:
+			c1.desce();
+			break;
+			
+		case KeyEvent.VK_A:
+			c2.avanca();
+			break;
+		case KeyEvent.VK_D:
+			c2.recua();
+			break;
+		case KeyEvent.VK_S:
+			c2.desce();
+			break;
+		case KeyEvent.VK_W:
+			c2.sobe();
 			break;
 		}
 
